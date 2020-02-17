f34fb9d9da136e57067769b25cb96e0785f5b8ca
Adam Crowder <adam@adamcrowder.net>
2020-02-16 16:58:13 -0800

    update waifu client to filter out husbando results
    Signed-off-by: Adam Crowder <adam@adamcrowder.net>

--- lib/waifu_client.py
+++ lib/waifu_client.py
@@ -25,31 +25,38 @@ async def process_request(channel, trigger: str):
         channel: Discord channel object to send the response
         trigger: trigger word for this request
     """
-    r = requests.get('https://mywaifulist.moe/random')
-    # Handle bad response
-    if r.status_code < 200 or r.status_code >= 300:
-        await channel.send(error_message)
-        print('Warning: Response {} from mywaifulist'.format(r.status_code))
-        print(r.text)
-        return
-    try:
-        # Parse html for the waifu id
-        page = BeautifulSoup(r.text, 'html.parser')
-        waifu_id = page.find('waifu-core').get(':waifu-id')
-        # Now query the api for the waifu information
-        r = requests.get('https://mywaifulist.moe/api/waifu/{}'.format(waifu_id), headers={'X-Requested-With': 'XMLHttpRequest'})
+    # Python do-while. Will return out of loop when necessary
+    while True:
+        r = requests.get('https://mywaifulist.moe/random')
+        # Handle bad response
         if r.status_code < 200 or r.status_code >= 300:
             await channel.send(error_message)
-            print('Warning: Response {} from mywaifulist api'.format(r.status_code))
+            print('Warning: Response {} from mywaifulist'.format(r.status_code))
             print(r.text)
             return
-        print(r.text)
-        res = r.json()
-        await channel.send('Your {} is {} from {}\n{}'.format(trigger, res['data']['name'], res['data']['series']['name'], res['data']['display_picture']))
-        # Make sure to truncate the descripiton if it is too long for discord
-        if len(res['data']['description']) >= 1950:
-            res['data']['description'] = res['data']['description'][:1950] + '...'
-        await channel.send(res['data']['description'])
-    except Exception as e:
-        print(e)
-        await channel.send(error_message)
+        try:
+            # Parse html for the waifu id
+            page = BeautifulSoup(r.text, 'html.parser')
+            waifu_id = page.find('waifu-core').get(':waifu-id')
+            # Now query the api for the waifu information
+            r = requests.get('https://mywaifulist.moe/api/waifu/{}'.format(waifu_id), headers={'X-Requested-With': 'XMLHttpRequest'})
+            if r.status_code < 200 or r.status_code >= 300:
+                await channel.send(error_message)
+                print('Warning: Response {} from mywaifulist api'.format(r.status_code))
+                print(r.text)
+                return
+            print(r.text)
+            res = r.json()
+            # If result is a husbando, ignore this request and try again
+            if res['data'].get('husbando'):
+                continue
+            await channel.send('Your {} is {} from {}\n{}'.format(trigger, res['data']['name'], res['data']['series']['name'], res['data']['display_picture']))
+            # Make sure to truncate the descripiton if it is too long for discord
+            if len(res['data']['description']) >= 1950:
+                res['data']['description'] = res['data']['description'][:1950] + '...'
+            await channel.send(res['data']['description'])
+            return
+        except Exception as e:
+            print(e)
+            await channel.send(error_message)
+            return
